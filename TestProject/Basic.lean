5d1ea206ab9de0c4f9ab0416f15d8dfba9c35d1a
ppls-nd-prs <bramswaanen@gmail.com>
2024-11-28 11:44:03 +0100

    Made proof of thm2 in Basic.lean compile

--- TestProject/Basic.lean
+++ TestProject/Basic.lean
@@ -3,6 +3,7 @@ theorem thm1 (a b : Nat) : a = 0 → a + b = b := by
   rw [h]
   rw [Nat.zero_add]
 
-theorem thm2 (a b c : Nat) : a + b + c = b + c := by
-  rw [thm1]
-  rw [thm1]
+theorem thm2 (a b c : Nat) : a = 0 → a + b + c = b + c := by
+  intro h
+  rw [h]
+  rw [Nat.zero_add]
