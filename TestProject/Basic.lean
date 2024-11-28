7395f92cbab6130c5f169dc8a38edb2d5b09320b
ppls-nd-prs <bramswaanen@gmail.com>
2024-11-28 11:26:39 +0100

    Added real proof to thm1 in Basic.lean

--- TestProject/Basic.lean
+++ TestProject/Basic.lean
@@ -1,5 +1,7 @@
-theorem thm1 (a b : Nat) : a + b = b := by
-  sorry
+theorem thm1 (a b : Nat) : a = 0 → a + b = b := by
+  intro h
+  rw [h]
+  rw [Nat.zero_add]
 
 theorem thm2 (a b c : Nat) : a + b + c = b + c := by
   rw [thm1]
