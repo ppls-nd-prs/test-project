30e1f0c81c26e4165db7fa938a0977a9f002100f
ppls-nd-prs <bramswaanen@gmail.com>
2024-11-28 10:28:42 +0100

    Editted content.tex and Basic.lean

--- TestProject/Basic.lean
+++ TestProject/Basic.lean
@@ -1,5 +1,6 @@
-theorem stuff (a b : Prop) : a ∧ b → b ∧ a := by
-  intro h
-  apply And.intro
-  exact (And.right h)
-  exact (And.left h)
+theorem thm1 (a b : Nat) : a + b = b := by
+  sorry
+
+theorem thm2 (a b c : Nat) : a + b + c = b + c := by
+  rw [thm1]
+  rw [thm1]
